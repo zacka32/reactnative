d532034c136c44c50d30b0386035048599066f72
Ahmad Zakaria <zakaria@concord-consulting.com>
2017-06-13 11:46:42 +0700

    add page

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './AuthReducer';
 import EmployeeFromReducer from './EmployeeFormReducer';
+import EmployeeReducer from './EmployeeReducer';
 
 export default combineReducers({
   auth: AuthReducer,
-  employeeForm: EmployeeFromReducer
+  employeeForm: EmployeeFromReducer,
+  employees: EmployeeReducer
 });
